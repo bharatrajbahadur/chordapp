1b770e02e49b2b6133abb7522e2e511852d98bc6
Bharat Raj Bahadur <brat.bahadur@gmail.com>
2013-11-29 13:40:25 -0500

    including links to song page

--- ChordApp/urls.py
+++ ChordApp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^songchord/',include('songchord.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^song_page/',include('songchord.urls')),
 )
